388d957be344b0e670bbc5f16d8094f7a530cbf2
anonymous <shyeyian@petalmail.com>
2025-12-25 00:21:11 +0800

    remain gnome only

--- gnome-auto-dark-mode/install.sh
+++
@@ -1,4 +0,0 @@
-mkdir -p ~/.config/systemd/user
-cp gnome-auto-dark-mode.timer   ~/.config/systemd/user/gnome-auto-dark-mode.timer
-cp gnome-auto-dark-mode.service ~/.config/systemd/user/gnome-auto-dark-mode.service
-systemctl --user enable --now gnome-auto-dark-mode.timer
--- install.sh
+++ install.sh
@@ -1,9 +1,4 @@
-cat .gitignore | xargs -I {} sh -c "rm -rf {}" 
-
-makepkg -D compute-meta   -si --noconfirm
-makepkg -D cpp-meta       -si --noconfirm
-makepkg -D gnome-meta     -si --noconfirm
-makepkg -D minecraft-meta -si --noconfirm
-makepkg -D paru-meta      -si --noconfirm
-makepkg -D system-meta    -si --noconfirm
-sh -c "cd gnome-auto-dark-mode && ./install.sh"
+mkdir -p ~/.config/systemd/user
+cp gnome-auto-dark-mode.timer   ~/.config/systemd/user/gnome-auto-dark-mode.timer
+cp gnome-auto-dark-mode.service ~/.config/systemd/user/gnome-auto-dark-mode.service
+systemctl --user enable --now gnome-auto-dark-mode.timer
